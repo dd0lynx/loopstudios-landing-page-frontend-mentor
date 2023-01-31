426a1dd427c0eb29274137dbdebaa555f30e51ed
dd0lynx <danieldazure@gmail.com>
2023-01-31 06:02:16 -0800

    trying stuff

--- gh-pages.js
+++ gh-pages.js
@@ -6,7 +6,7 @@ publish(
         branch: 'gh-pages',
         repo: 'https://github.com/dd0lynx/loopstudios-landing-page-frontend-mentor', // Update to point to your repository  
         user: {
-            name: 'dd0lynx', // update to use your name
+            name: 'Daniel D', // update to use your name
             email: 'danieldazure@gmail.com' // Update to use your email
         }
     },
