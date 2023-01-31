57dbe498e7c52b93895a0ca01581c6972f8c688d
dd0lynx <danieldazure@gmail.com>
2023-01-31 05:23:56 -0800

    minor layout fixes. updated readme

--- src/App.svelte
+++ src/App.svelte
@@ -86,11 +86,12 @@
     </div>
 
     <div class="copyright">
-        © 2021 Loopstudios. All rights reserved.
+        <p>© 2021 Loopstudios. All rights reserved.</p>
+        
     
         <div class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw">Frontend Mentor</a>. 
-            Coded by <a href="https://www.frontendmentor.io/profile/dd0lynx">Daniel Dowty</a>.
+            <p>Challenge by <a href="https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw">Frontend Mentor</a>.
+            Coded by <a href="https://www.frontendmentor.io/profile/dd0lynx">Daniel Dowty</a>.</p>
         </div>
     </div>
 
